5f14bced74974d6e61de389e102bd6050d2ae024
Juhan Heinlaid <98657402+D1ZEASE420@users.noreply.github.com>
2024-04-11 13:01:40 +0300

    Delete genipea.js

--- genipea.js
+++
@@ -1,8 +0,0 @@
-
-const carBrands = [
-    "Toyota", "Honda", "Ford", "Chevrolet", "Nissan", "BMW", "Mercedes-Benz", "Audi", "Volkswagen", "Hyundai", "Kia", "Mazda", "Tesla", "Subaru", "Lexus", "Jeep", "Chrysler", "Dodge", "Volvo", "Jaguar", "Land Rover", "Porsche", "Ferrari", "Maserati",
-  ];
-  for (let i = 0; i < carBrands.length; i++) {
-    console.log(carBrands[i]);
-  }
-  
